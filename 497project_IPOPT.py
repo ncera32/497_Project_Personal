975d84fa51fa67e2a28799d27878ee6ad9016362
ncera32 <125378778+ncera32@users.noreply.github.com>
2024-04-26 19:38:22 -0400

    Update 497project_IPOPT.py

--- 497project_IPOPT.py
+++ 497project_IPOPT.py
@@ -209,7 +209,6 @@ optOptions = { "print_level": [int, 0],
                 "print_user_options": [str, "yes"],
                 "output_file": os.path.join(outputDir, "SLSQP.out"),
                 "linear_solver": [str, "mumps"],
-                "Tol": 1e-6
              }
 opt = OPT("IPOPT", options=optOptions)
 sol = opt(optProb, MP.sens, storeHistory=os.path.join(outputDir, "opt_IPOPT.hst"))
